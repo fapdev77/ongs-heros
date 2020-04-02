804dbc3827693d5ebfa06d53a2e7402e7b1056e0
Fabio <fapdev77@gmail.com>
2020-04-02 13:38:18 -0300

    Finalizando aplicativo movel.

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+//importar componente com as rotas do arquivo de rotas
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
+
