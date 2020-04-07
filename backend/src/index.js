abfe60bf875c17c38db2017a4031b61d51f96f04
Fabio <fapdev77@gmail.com>
2020-04-07 14:37:17 -0300

    Add env port configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const PORT = process.env.PORT || 5000;
 // Importar as funcoes do express dentro da constante app
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use(routes);
 
 
-// Escutar na porta 3333 as requisições
-app.listen(3333);
+// Escutar na porta configurada no arquivo .env ou na porta 5000 caso não exista o arquivo de config
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
